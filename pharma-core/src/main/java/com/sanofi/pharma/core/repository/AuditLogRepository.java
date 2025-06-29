c817cb7561f83f322dc5ff19774f10dc532ba346
jli365 <jin.li.4@volvocars.com>
2025-06-29 10:13:11 +0800

    auditlog

--- pharma-core/src/main/java/com/sanofi/pharma/core/repository/AuditLogRepository.java
+++ pharma-core/src/main/java/com/sanofi/pharma/core/repository/AuditLogRepository.java
@@ -2,9 +2,11 @@ package com.sanofi.pharma.core.repository;
 
 import com.sanofi.pharma.core.entity.AuditLog;
 import com.sanofi.pharma.core.entity.Drug;
+import com.sanofi.pharma.core.entity.PharmacyDrugRelationship;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long>, JpaSpecificationExecutor<AuditLog> {
 }
